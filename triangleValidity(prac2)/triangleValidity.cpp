074d480e63eda299a26d6df74651dc793b16470e
kehvy <khushbu.vijay394@yahoo.com>
2013-03-26 15:57:46 +0530

    checks validity of triangle

---
+++ triangleValidity(prac2)/triangleValidity.cpp
@@ -0,0 +1,32 @@
+/*
+ * triangleValidity.cpp
+ *	checks validity of a triangle
+ *  Created on: Mar 26, 2013
+ *      Author: Vijaywargi
+ */
+
+
+
+
+
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int main()
+{
+	int a,b,c,sum;
+
+	cout<<"Enter the value of all three angles of a triangle"<<endl;
+	cout<<"a:";
+	cin>>a;
+	cout<<"b:";
+	cin>>b;
+	cout<<"c:";
+	cin>>c;
+	sum=a+b+c;
+	if(sum == 180)					//sum of all the angles of a triangle is 180.
+		cout<<"Valid triangle";
+	else
+		cout<<"Invalid triangle";
+	return 0;
+}
